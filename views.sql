8356781e758458fc3004eff7db86ee7f7359b363
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2025-06-13 11:38:32 -0400

    Update views.sql

--- views.sql
+++ views.sql
@@ -3,6 +3,7 @@ AS
 select R.UUID, R.FILENAME, R.JSON_DATA, 
        get_presigned_url(@TRAFFIC, R.FILENAME) as IMG_URL,
        N.CREATED_TS, N.DIRECTIONOFTRAVEL, N.ENDING, N.LATITUDE, N.longitude, N.ROADWAYNAME,
-       N.URL2, N.VIDEOID, N.VIDEONAME, DT.relative_path, DT.size as file_size, DT.last_modified, 
+       N.URL2, N.VIDEOID, N.VIDEONAME, DT.relative_path, DT.size as file_size, DT.last_modified
 from RAWNYCTRAFFICIMAGES R LEFT OUTER JOIN nyctrafficimages N ON R.FILENAME = N.FILENAME
-     INNER JOIN DIRECTORY(@TRAFFIC) DT ON DT.relative_path = N.FILENAME;
+     INNER JOIN DIRECTORY(@TRAFFIC) DT ON DT.relative_path = N.FILENAME
+     order by DT.last_modified desc;
